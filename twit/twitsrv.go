45163b6697bcf85b29d2c275dd4582cc1982f9e7
David Leimbach <leimy2k@gmail.com>
2015-04-26 16:25:57 -0700

    Better buffer handling.  Aggregates all writes to a per-FFID buffer then tweets at Clunk time

--- twit/twitsrv.go
+++ twit/twitsrv.go
@@ -3,73 +3,50 @@
 package main
 
 import (
+	"flag"
+	twit "github.com/Leimy/icy-metago/twitter"
 	"github.com/mortdeus/go9p"
 	"github.com/mortdeus/go9p/srv"
-	"flag"
 	"log"
 	"os"
-	twit "github.com/Leimy/icy-metago/twitter"
 )
+
 type Twitfs struct {
-	srv *srv.Fsrv
-	user go9p.User
+	srv   *srv.Fsrv
+	user  go9p.User
 	group go9p.Group
-	blksz int
-	blkchan chan []byte
-	zero []byte // 0s
 }
 
 type TweetFile struct {
 	srv.File
-	data [][]byte
+	data []byte
 }
 
 var addr = flag.String("addr", "./crustysock", "unix domain socket path")
 var debug = flag.Int("d", 0, "debuglevel")
-var blksize = flag.Int("b", 200, "block size") // likely enough, twitter y'know
 var logsz = flag.Int("l", 2048, "log size")
 var tsrv Twitfs
 
-func (t *TweetFile) expand(sz uint64) {
-	blknum := sz / uint64(tsrv.blksz)
-	if sz & uint64(tsrv.blksz) != 0 {
-		blknum++
-	}
 
-	data :=make([][]byte, blknum)
-	if t.data != nil {
-		copy(data, t.data)
-	}
-	t.data = data
-	t.Length = sz
-}
-
-// Ripped off from ramfs
+// technically we should be reading the data into a buffer for this file
+// and when it gets Clunked, send the message
 func (t *TweetFile) Write(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
-	go twit.Tweet(string(buf))
-	
+	t.data = append(t.data, buf...)
 	return len(buf), nil
 }
 
-// func (t *TweetFile) Clunk(req *srv.Req) {
-// defer t.Unlock()
-// 	var tweetbody []byte
-// 	for _, chunk := range t.data {
-// 		tweetbody = append(tweetbody, string(chunk)...)
-// 	}
-// 	twit.Tweet(string(tweetbody))
-// 	req.RespondRclunk()
-// }
+func (t *TweetFile) Clunk(fid *srv.FFid) error {
+	go twit.Tweet(string(t.data))
+	log.Printf("Clunk: %p\n", fid)
+	return nil
+}
 
-func main () {
+func main() {
 	flag.Parse()
-	
+
 	tsrv.user = go9p.OsUsers.Uid2User(os.Geteuid())
 	tsrv.group = go9p.OsUsers.Gid2Group(os.Getegid())
-	tsrv.blksz = *blksize
-	tsrv.blkchan = make(chan []byte, 200)
-	tsrv.zero = make([]byte, tsrv.blksz)
-	
+
 	root := new(srv.File)
 	err := root.Add(nil, "/", tsrv.user, nil, go9p.DMDIR|0555, nil)
 	if err != nil {
@@ -77,21 +54,21 @@ func main () {
 	}
 
 	twitterentry := new(TweetFile)
-	err = twitterentry.Add(root, "twat", go9p.OsUsers.Uid2User(os.Geteuid()), nil, 0600, twitterentry)
+	err = twitterentry.Add(root, "tweet", go9p.OsUsers.Uid2User(os.Geteuid()), nil, 0600, twitterentry)
 	if err != nil {
 		log.Panic(err)
 	}
-	
+
 	l := go9p.NewLogger(*logsz)
-	
+
 	tsrv.srv = srv.NewFileSrv(root)
-	tsrv.srv.Dotu = true // 9p2000
+	tsrv.srv.Dotu = true // 9p2000.u
 	tsrv.srv.Debuglevel = *debug
-	
+
 	tsrv.srv.Start(tsrv.srv)
 	tsrv.srv.Id = "tweetfs"
 	tsrv.srv.Log = l
-	
+
 	err = tsrv.srv.StartNetListener("unix", *addr)
 	if err != nil {
 		log.Panic(err)
