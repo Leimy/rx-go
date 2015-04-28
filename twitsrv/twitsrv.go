85b5ff31255499363e50ea8b3321c56e3fd3d5cd
David Leimbach <leimy2k@gmail.com>
2015-04-27 21:05:31 -0700

    Added ability to add new tweet endpoints

--- twitsrv/twitsrv.go
+++ twitsrv/twitsrv.go
@@ -9,6 +9,7 @@ import (
 	"github.com/mortdeus/go9p/srv"
 	"log"
 	"os"
+	"strings"
 )
 
 type Twitfs struct {
@@ -21,6 +22,11 @@ type TweetFile struct {
 	srv.File
 	data []byte
 	tweeter twit.Tweeter
+}
+
+type TweetFileFactory struct {
+	srv.File
+	data []byte
 }
 
 func NewTweetFile(root *srv.File, name string, user go9p.User, group go9p.Group, mode uint32, template string) (error, *TweetFile) {
@@ -68,6 +74,26 @@ func (t *TweetFile) Clunk(fid *srv.FFid) error {
 	return nil
 }
 
+func (tff *TweetFileFactory) Write(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
+	tff.data = append(tff.data, buf...)
+	return len(buf), nil
+}
+
+func (tff *TweetFileFactory) Clunk(fid *srv.FFid) error {
+	s := string(tff.data)
+	all := strings.SplitN(s, "|", 2)
+	
+	if len(all) != 2 {
+		log.Printf("Illegal request, ignoring: %s\n", s)
+	} else {
+		if err, _ := NewTweetFile(root, all[0], tsrv.user, tsrv.group, 0600, all[1]); err != nil {
+			log.Printf("Failed to allocate: %s for %s\n", all[0], all[1])
+		}
+	}
+	return nil
+}
+
+
 func start_service () {
 	l := go9p.NewLogger(*logsz)
 
@@ -86,6 +112,12 @@ func start_service () {
 }
 
 func main() {
+	
+	tff := new(TweetFileFactory)
+	if err := tff.Add(root, "creator", tsrv.user, tsrv.group, 0600, tff); err != nil {
+		log.Panicf("Failed to create the creator: %v\n", err)
+	}
+	
 	if err, _ := NewTweetFile(root, "metadata", tsrv.user, tsrv.group, 0600, METADATA_SFX); err != nil {
 		log.Panicf("Failed to allocate metadata endpoint: %v\n", err)
 	}
