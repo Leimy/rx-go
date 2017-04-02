91113f0ab02f2ace7641219ce14568878b2ddb08
David Leimbach <leimy2k@gmail.com>
2017-04-02 10:22:18 -0700

    no local state for no good reason

--- tweetit/main.go
+++ tweetit/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/Leimy/rx-go/twit"
-import "os"
+import (
+	"bufio"
+	"github.com/Leimy/rx-go/twit"
+	"os"
+)
 
-func main () {
-	twit.MakeTweeter("http://tunein.com/radio/Radio-Xenu-s118981/ @radioxenu")(os.Args[1])
+func condTweet(line string, err error) func(twit.Tweeter) {
+	if err == nil {
+		return func(t twit.Tweeter) {
+			t(line)
+		}
+	}
+	return func(_ twit.Tweeter) {}
+}
+
+func main() {
+	condTweet(bufio.NewReader(os.Stdin).ReadString('\n'))(twit.MakeTweeter("http://tunein.com/radio/Radio-Xenu-s118981/ @radioxenu"))
 }
